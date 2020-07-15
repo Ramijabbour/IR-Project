a7b74e45d58f92d37be07316f3a065f6662f8e46
Rami Jabbour <ramijab.565@gmail.com>
2020-07-15 12:53:14 -0400

    add project

---
+++ src/test/java/com/example/IR/Project/IrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IR.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
